4fc6d873297559be90e0a8c8dc4017d743382a95
Daniel Alejandro <outnumbered@live.com>
2020-08-22 05:59:37 -0500

    update

--- 8- download finantial statements/get_finantial_docs.py
+++ 8- download finantial statements/get_finantial_docs.py
@@ -7,7 +7,7 @@ def get_fin_docs(CIK="1018724"):
 
     # define our parameters dictionary
     param_dict = {'action':'getcompany',
-                'CIK':'1018724',
+                'CIK': CIK,
                 'type':'10-Q',
                 'dateb':'',
                 'owner':'exclude',
@@ -25,36 +25,37 @@ def get_fin_docs(CIK="1018724"):
     # this list will save all interative data links
     I_D_links = []
 
-    # get all interactive data financial statements
+    # get the table that have the class tableFile2
     table = soup.find(class_="tableFile2")
 
-    rows = table.find_all("tr")[1:]
+    rows = table.find_all("tr")[1:] #removes the header row
+
+    # iterates al rows of the table
     for row in rows:
         l = []
         s = "interactiveDataBtn"
+
+        # if interactiveDataBtn text exist in the row
         if s in row.decode():
-            date = row.find_all("td")[3].string
-            url = row.find(id=s).get('href')
+            date = row.find_all("td")[3].string # get the date
+            url = row.find(id=s).get('href') # get the interactive data url
             l.append("https://www.sec.gov" + url)
             l.append(date)
             I_D_links.append(l)
-    print(I_D_links)
 
-    F_S_links = []
+    # getting the xls finantial docs
     len_docs = len(I_D_links)
     print("Gettings docs URLs... ")
     for n, l in enumerate(I_D_links):
-        print(str(int(100*n/len_docs)) + "%...")
+        print(str(int(100*n/len_docs)) + "%...") # print the percentage of the process
         endpoint = l[0]
-        l2 = []
         response = requests.get(url = endpoint)
         soup = BeautifulSoup(response.content, 'lxml')
+        # look for the xls docs links
         entries = soup.find("a", string="View Excel Document")
         url = entries.get('href')
-        l2.append("https://www.sec.gov" + url)
-        l2.append(l[1])
-        F_S_links.append(l2)
-    return F_S_links
+        I_D_links[n].append("https://www.sec.gov" + url)
+    return I_D_links
 
 get_fin_docs()
 d = get_fin_docs()
